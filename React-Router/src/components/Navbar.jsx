26eb3cc926a9d40e8bc3af9bb5a9d8f8b803bf90
Apple <apple@Apples-MacBook-Pro.local>
2026-03-03 00:44:39 +0530

    update

---
+++ React-Router/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import logo from '../assets/images/logo.png'
+
+function Nav_bar() {
+    return (
+        <Navbar bg="light" data-bs-theme="light">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <img
+                        src={logo}
+                        alt="Logo"
+                        width="185"
+                        height="70"
+                        className="d-inline-block align-top me-2"
+                    />
+                </Navbar.Brand>
+                <Nav className="ms-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="/addbook">Add Book</Nav.Link>
+                    <Nav.Link as={Link} to="/viewbook">View Books</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+export default Nav_bar;
---
+++ React-Router/src/pages/Home.jsx
@@ -0,0 +1,78 @@
+import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import img1 from '../assets/images/slide1.png'
+import img2 from '../assets/images/slide2.png'
+
+function Home({ books }) {
+
+    return (
+        <>
+            {/* section 1 */}
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img1}
+                        alt="Slide 1"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src={img2}
+                        alt="Slide 2"
+                    />
+                </Carousel.Item>
+            </Carousel>
+
+            {/* section 2 */}
+
+            <Container fluid className="book-section mt-5">
+                <Container>
+                <h2 className="text-center mb-4">Our Books</h2>
+
+                <Row>
+                    {books.map((book) => (
+                        <Col md={4} className="mb-4" key={book.b_id}>
+                            <Card className="h-100 h-100 book-card">
+
+                                {/* Book Image */}
+                                <Card.Img
+                                    variant="top"
+                                    src={book.image}
+                                    style={{ height: "400px", objectFit: "cover" }}
+                                />
+
+                                <Card.Body>
+                                    <Card.Title>{book.title}</Card.Title>
+
+                                    <Card.Text>
+                                        <strong>Author:</strong> {book.author} <br />
+                                        <strong>Price:</strong> ₹{book.price}
+                                    </Card.Text>
+
+                                    {/* Short Description */}
+                                    <Card.Text style={{ fontSize: "14px" }}>
+                                        {book.des.substring(0, 120)}...
+                                    </Card.Text>
+
+                                    <Button className='custom-btn'>
+                                        View Details
+                                    </Button>
+
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </Container>
+            </Container>
+        </>
+    );
+}
+
+export default Home;
